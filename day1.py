22a731fb0b6f5047828c6c6714ba119c4dc46877
Andrés Cruz <andrescruz@Andress-MacBook-Pro.local>
2022-04-11 08:23:56 -0600

    day1

---
+++ day1.py
@@ -0,0 +1,25 @@
+# # day 1 print
+# print("Hello World")
+# ## String manipulation and Code Intelligence
+# print("Hello World\nHello World")
+# print("Hello"+" "+"Andres")
+# ##Python Input Function
+# #input obtendra primero la informacion de consola, luego se imprimira la cadena de texto
+# print("Hello "+input("What is your name? ")+"!")
+# ##Python Variables
+# #len() permite recuperar el tamaño de un string
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+# ##Variable Names
+# #no colocar variables con espacios user_name es valido user name no lo es
+# #las variables no pueden iniciar con numeros
+# #no usar palabras reservadas como nombres de variables
+
+##Band name generator
+print('Welcome to the Band Name Generator.')
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet)
